a45b01f08626ab7bd64e3bb942b84117ca75aeef
Md Monirul Islam <mmislam027@gmail.com>
2021-07-31 15:15:07 +0600

    course:featured course responsive

--- src/components/course/featured/AllFeaturedCourses.js
+++ src/components/course/featured/AllFeaturedCourses.js
@@ -68,6 +68,27 @@ const AllFeaturedCourses = () => {
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 776,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 460,
+        settings: {
+          slidesToShow: 1,
+          arrows: false,
+        },
+      },
+    ],
   };
   return (
     <div className={style.root + ' featured'}>
