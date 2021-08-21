181cb6ee0e5e3ca5f4cdd971386062a75f480717
Md Monirul Islam <mmislam027@gmail.com>
2021-08-21 20:00:15 +0000

    breadcrup done

--- src/components/breadcrumb/Breadcrumb.js
+++ src/components/breadcrumb/Breadcrumb.js
@@ -1,17 +1,16 @@
 import Background from 'images/breadcrumb/1.png';
 import React, { useEffect } from 'react';
-import style from './Breadcrumb.module.css';
 
 const Breadcrumb = ({ title }) => {
   useEffect(() => (document.title = title), [title]);
   return (
     <div
-      className={style.root}
+      className='bg-cover bg-center bg-no-repeat h-72 flex justify-center items-center'
       style={{
         background: `linear-gradient(90deg, #3580ebe6 , #3580ebe6) , url(${Background})`,
       }}
     >
-      <h2 className={style.title}>{title}</h2>
+      <h2 className='text-5xl text-white'>{title}</h2>
     </div>
   );
 };
