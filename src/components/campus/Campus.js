7c33e4f4c57badb6c22dfe91a6c3ecf9e1b0fb9b
Md Monirul Islam <mmislam027@gmail.com>
2021-08-21 20:07:32 +0000

    campus section done

--- src/components/campus/Campus.js
+++ src/components/campus/Campus.js
@@ -1,30 +1,30 @@
 import '@fortawesome/fontawesome-free/css/all.css';
+import { Container } from 'components/styles/Container.styled';
 import { Title } from 'components/styles/Section.styled';
 import Bg from 'images/campus-bg.png';
 import React from 'react';
-import style from './Campus.module.css';
 const Campus = () => {
   return (
     <div
-      className={style.root}
+      className='py-24 bg-no-repeat bg-center bg-cover text-center'
       style={{
         backgroundImage: `linear-gradient(90deg ,rgb(69 128 237 / 80%) ,rgb(236 115 206 / 82%)) , url(${Bg})`,
       }}
     >
-      <div className='container'>
+      <Container>
         <Title align='center' white>
           Watch Campus Life Video Tour
         </Title>
-        <p className={style.subTitle}>
+        <p className='text-xl w-full sm:w-9/12 mx-auto text-white leading-7 mb-10'>
           we believe everyone should have the to create progress through
           technology and develop the skills of tomorrow. assessments, learning
         </p>
-        <div className={style.video}>
-          <div className={style.icon}>
+        <div className='bg-gray-300 h-28 w-28 rounded-full flex justify-center items-center m-auto border-8 border-gray-400 box-border cursor-pointer'>
+          <div className='text-2xl text-blue-500'>
             <i className='fas fa-play'></i>
           </div>
         </div>
-      </div>
+      </Container>
     </div>
   );
 };
