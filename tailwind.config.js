da2cecdee0c5549ae1866cc88bcefd06e425f95b
Md Monirul Islam <mmislam027@gmail.com>
2021-08-23 17:38:00 +0000

    default color black changed to #120f2d

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const { colors: defaultColors } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      ...defaultColors,
+      black: '#120f2d',
+    },
   },
   variants: {
     extend: {},
