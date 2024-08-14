9ea9b1b1be1c67e6f337e1aae9f481efef9b5be7
Monirul Islam <mmislam027@gmail.com>
2024-08-14 22:58:40 +0600

    update home js for production build

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,16 @@
-import About from 'components/about/About';
-import Blogs from 'components/blog/Blogs';
-import Campus from 'components/campus/Campus';
-import HomeCourses from 'components/course/Courses/HomeCourses';
-import AllFeaturedCourses from 'components/course/featured/AllFeaturedCourses';
-import Facility from 'components/facility/Facility';
-import Fact from 'components/fact/Fact';
-import Hero from 'components/hero/Hero';
-import Instructor from 'components/instructor/Instructor';
-import Testimonials from 'components/testimonial/Testimonials';
-import Why from 'components/why/Why';
-import React from 'react';
-import { useEffect } from 'react/cjs/react.development';
+import About from "components/about/About";
+import Blogs from "components/blog/Blogs";
+import Campus from "components/campus/Campus";
+import HomeCourses from "components/course/Courses/HomeCourses";
+import AllFeaturedCourses from "components/course/featured/AllFeaturedCourses";
+import Facility from "components/facility/Facility";
+import Fact from "components/fact/Fact";
+import Hero from "components/hero/Hero";
+import Instructor from "components/instructor/Instructor";
+import Testimonials from "components/testimonial/Testimonials";
+import Why from "components/why/Why";
 
 const Home = () => {
-  useEffect(() => (document.title = 'Home'), []);
-
   return (
     <>
       <Hero />
